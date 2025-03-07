f5011383968d367f24e3664ff9f67527f046cdc7
VitaliiKuryvchak <vitalikkuryvcak@gmail.com>
2025-03-07 09:45:18 +0200

    Update Main.java

--- Lab2java/src/Main.java
+++ Lab2java/src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class Lab2 {
+public class Main {
     public static void main(String[] args) {
         int size = 10; 
         double[] array = new double[size];
